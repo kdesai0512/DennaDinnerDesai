eb48c2591d935e1f82b93a4887c0709a5f708856
Shannon Denna <sdenna@d211.org>
2019-09-20 12:51:22 -0500

    Code at end of 5th period Fri, 9-19-19

--- app/src/main/java/com/example/dennadinnerp5/Food.java
+++ app/src/main/java/com/example/dennadinnerp5/Food.java
@@ -14,4 +14,48 @@ public class Food {
         this.imageResourceID = imageResourceID;
     }
 
+    public static final Food[] breakfastFoods = {
+            new Food("Eggs", "3 eggs, 1 cheese, 1 meat omelet",
+                    8.99, R.drawable.omelet),
+            new Food("Pancakes", "3 pancakes, choice of meat, potato",
+                    7.95, R.drawable.pancakes),
+            new Food("Waffles", "Belgium waffles, whip cream, fresh fruit",
+                    7.50, R.drawable.waffles)
+    };
+
+    public String toString() {
+        return foodName;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
 }
--- app/src/main/java/com/example/dennadinnerp5/MainActivity.java
+++ app/src/main/java/com/example/dennadinnerp5/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.dennadinnerp5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listView,
+                            View itemView, int position, long id){
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                    BreakfastChoicesActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                };
+        //Add the listener to the list View
+        ListView listView = (ListView) findViewById(R.id.mealChoices);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
